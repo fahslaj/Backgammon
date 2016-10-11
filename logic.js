526521e3df9a4b3e3404812162c773848b8196f1
fahslaj <fahslaj@rose-hulman.edu>
2016-10-10 21:32:45 -0400

    more game logic

--- logic.js
+++ logic.js
@@ -1,6 +1,10 @@
 var GameState = {
 	turn: 0,
-	dice: [-1, -1],
+	bearing: 0,
+	dice: {
+		1: [-1, 0],
+		2: [-1, 0]
+	},
 	board: {
 		triangles: [],
 		bar: []
@@ -30,5 +34,49 @@ for (var i = 0; i < 24; i++) {
 }
 
 function rollDie(diePosition) {
-	dice[diePosition] = Math.floor(Math.random()*6) + 1;
+	GameState.dice[diePosition][0] = Math.floor(Math.random()*6) + 1;
+	GameState.dice[diePosition][1] = 0;
+}
+
+function isValidMove(startTriangle, endTriangle) {
+	var player = GameState.turn;
+	if (GameState.board.triangles[startTriangle].length >= 1 &&
+		GameState.board.triangles[startTriangle].indexOf(player) != -1 &&
+		GameState.board.triangles[endTriangle].length <= 4 &&
+		GameState.board.triangles[endTriangle].indexOf(!player) == -1) {
+		if (endTriangle - startTriangle == GameState.dice[0][0] &&
+			!GameState.dice[0][1]) {
+			return 1;
+		}
+		if (endTriangle - startTriangle == GameState.dice[1][0] &&
+			!GameState.dice[1][1]) {
+			return 2;
+		} // TODO: check if uses both dice
+	}
+	return false;
+}
+
+function makeMove(startTriangle, endTriangle) {
+	var die = isValidMove(startTriangle, endTriangle);
+	if (!die) {
+		throw 'Invalid move';
+	}
+	GameState.board.triangles[endTriangle].push(
+		GameState.board.triangles[startTriangle].pop());
+	GameState.dice[die][1] = 1;
+	if (GameState.dice[0][1] && GameState.dice[1][1]) {
+		newTurn(); // TODO: change to next turn button
+	}
+}
+
+function newTurn() {
+	if (!checkWin()) {
+		rollDie(0);
+		rollDie(1);
+		GameState.turn = !GameState.turn;
+	}
+}
+
+function checkWin() {
+	// check for win conditions
 }
