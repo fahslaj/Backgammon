d59c0f7839644d932a704c626449ad1d85ade998
Joel Shapiro <joel.shapiro7@gmail.com>
2016-10-11 22:54:14 -0400

    code cleanup

--- backgammon.js
+++ backgammon.js
@@ -1,3 +1,6 @@
+/** @namespace gl.DEPTH_TEST */
+/** @namespace gl.ARRAY_BUFFER */
+
 var canvas;
 var gl;
 var colorLoc;
@@ -14,10 +17,10 @@ var vertices = [];
 // colors is the actual list of colors
 var colors = [];
 
-// indices defines the list of this lists of indeces which define each polygon
+// indices defines the list of this lists of indices which define each polygon
 var indices = [];
-// colorIndeces defines the color index for each polygon
-var colorIndeces = [];
+// colorIndices defines the color index for each polygon
+var colorIndices = [];
 
 var theta = [];
 
@@ -37,50 +40,50 @@ var aspect;
 window.onload = function init() {
     canvas = document.getElementById("gl-canvas");
 
-	/** @type {WebGLRenderingContext} */
+    /** @type {WebGLRenderingContext} */
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) { alert("WebGL isn't available"); }
 
-	// load base colors
+    // load base colors
     colors.push(
-		vec4(1.0, 0.0, 0.0, 1.0),  // red
-		vec4(1.0, 1.0, 0.0, 1.0),  // yellow
-		vec4(0.0, 1.0, 0.0, 1.0),  // green
-		vec4(0.0, 0.0, 1.0, 1.0),  // blue
-		vec4(0.0, 1.0, 1.0, 1.0),  // cyan
-		vec4(1.0, 0.0, 1.0, 1.0)   // magenta
-	);
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+        vec4(1.0, 0.0, 1.0, 1.0)   // magenta
+    );
 
-	// world rotation
-	theta[0] = 0.0;
-	theta[1] = 0.0;
-	theta[2] = 0.0;
+    // world rotation
+    theta[0] = 0.0;
+    theta[1] = 0.0;
+    theta[2] = 0.0;
 
 
 
     // TODO: replace these calls which hard set the values with game logic which places objects here
-	addObject(
-		[
-			vec4(0, 0, cubeSize, 1),
-			vec4(0, cubeSize, cubeSize, 1),
-			vec4(cubeSize, cubeSize, cubeSize, 1),
-			vec4(cubeSize, 0.0, cubeSize, 1),
-			vec4(0, 0, 0, 1),
-			vec4(0, cubeSize, 0, 1),
-			vec4(cubeSize, cubeSize, 0, 1),
-			vec4(cubeSize, 0, 0, 1)
-		],
-		[
-			[1, 0, 3, 3, 2, 1],  // front face
-			[2, 3, 7, 6],  		 // right face
-			[3, 0, 4, 4, 7],  	 // bottom face
-			[6, 5, 1, 1, 2, 6],  // top face
-			[4, 5, 6, 6, 7, 4],  // back face
-			[5, 4, 0, 0, 1, 5]   // left face
-		],
-		[
-			0, 1, 2, 3, 4, 5
-		]);
+    addObject(
+        [
+            vec4(0, 0, cubeSize, 1),
+            vec4(0, cubeSize, cubeSize, 1),
+            vec4(cubeSize, cubeSize, cubeSize, 1),
+            vec4(cubeSize, 0.0, cubeSize, 1),
+            vec4(0, 0, 0, 1),
+            vec4(0, cubeSize, 0, 1),
+            vec4(cubeSize, cubeSize, 0, 1),
+            vec4(cubeSize, 0, 0, 1)
+        ],
+        [
+            [1, 0, 3, 3, 2, 1],  // front face
+            [2, 3, 7, 6],  		 // right face
+            [3, 0, 4, 4, 7],  	 // bottom face
+            [6, 5, 1, 1, 2, 6],  // top face
+            [4, 5, 6, 6, 7, 4],  // back face
+            [5, 4, 0, 0, 1, 5]   // left face
+        ],
+        [
+            0, 1, 2, 3, 4, 5
+        ]);
 
 
 
@@ -88,39 +91,39 @@ window.onload = function init() {
     //  Configure WebGL
     //
     gl.viewport(0, 0, canvas.width, canvas.height);
-	aspect = canvas.width / canvas.height;
+    aspect = canvas.width / canvas.height;
     gl.clearColor(1, 1, 1, 1);
-	gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.DEPTH_TEST);
 
     //  Load shaders and initialize attribute buffers
     program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-	colorLoc = gl.getUniformLocation(program, "color");
-	modelViewLoc = gl.getUniformLocation(program, "modelView");
-	projectionLoc = gl.getUniformLocation(program, "projection");
+    colorLoc = gl.getUniformLocation(program, "color");
+    modelViewLoc = gl.getUniformLocation(program, "modelView");
+    projectionLoc = gl.getUniformLocation(program, "projection");
 
     vBuffer = gl.createBuffer();
     vPosition = gl.getAttribLocation(program, "vPosition");
-	iBuffer = gl.createBuffer();
+    iBuffer = gl.createBuffer();
 
     render();
 };
 
 /**
- * Adds a new obejct made of polygons to be rendered.
+ * Adds a new object made of polygons to be rendered.
  *
- * Given indeces are relative to the new vertices, but colors are relative to the global color list
+ * Given indices are relative to the new vertices, but colors are relative to the global color list
  */
-function addObject(newVertices, newIndeces, newColorIndeces) {
-	var vertexOffset = vertices.length;
-	vertices = vertices.concat(newVertices);
-	indices = indices.concat(newIndeces.map(function(indexList) {
-		return indexList.map(function(elem) {
-			return elem + vertexOffset;
-		});
-	}));
-	colorIndeces = colorIndeces.concat(newColorIndeces);
+function addObject(newVertices, newIndices, newColorIndices) {
+    var vertexOffset = vertices.length;
+    vertices = vertices.concat(newVertices);
+    indices = indices.concat(newIndices.map(function(indexList) {
+        return indexList.map(function(elem) {
+            return elem + vertexOffset;
+        });
+    }));
+    colorIndices = colorIndices.concat(newColorIndices);
 }
 
 function render() {
@@ -133,60 +136,60 @@ function render() {
     gl.enableVertexAttribArray(vPosition);
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array([].concat.apply([], indices)), gl.STATIC_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array([].concat.apply([], indices)), gl.STATIC_DRAW);
 
     var c = [];
     var s = [];
-	for (i = 0; i < 3; i++) {
-		var a = radians(theta[i]);
-		c[i] = Math.cos(a);
-		s[i] = Math.sin(a);
-	}
+    for (i = 0; i < 3; i++) {
+        var a = radians(theta[i]);
+        c[i] = Math.cos(a);
+        s[i] = Math.sin(a);
+    }
 
-	var rx = mat4(1.0, 0.0, 0.0, 0.0,
-		0.0, c[0], -s[0], 0.0,
-		0.0, s[0], c[0], 0.0,
-		0.0, 0.0, 0.0, 1.0);
+    var rx = mat4(1.0, 0.0, 0.0, 0.0,
+        0.0, c[0], -s[0], 0.0,
+        0.0, s[0], c[0], 0.0,
+        0.0, 0.0, 0.0, 1.0);
 
-	var ry = mat4(c[1], 0.0, s[1], 0.0,
-		0.0, 1.0, 0.0, 0.0,
-		-s[1], 0.0, c[1], 0.0,
-		0.0, 0.0, 0.0, 1.0);
+    var ry = mat4(c[1], 0.0, s[1], 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        -s[1], 0.0, c[1], 0.0,
+        0.0, 0.0, 0.0, 1.0);
 
-	var rz = mat4(c[2], -s[2], 0.0, 0.0,
-		s[2], c[2], 0.0, 0.0,
-		0.0, 0.0, 1.0, 0.0,
-		0.0, 0.0, 0.0, 1.0);
+    var rz = mat4(c[2], -s[2], 0.0, 0.0,
+        s[2], c[2], 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0);
 
-	var tz1 = mat4(1.0, 0.0, 0.0, -cubeSize2,
-		0.0, 1.0, 0.0, -cubeSize2,
-		0.0, 0.0, 1.0, -cubeSize2,
-		0.0, 0.0, 0.0, 1.0);
+    var tz1 = mat4(1.0, 0.0, 0.0, -cubeSize2,
+        0.0, 1.0, 0.0, -cubeSize2,
+        0.0, 0.0, 1.0, -cubeSize2,
+        0.0, 0.0, 0.0, 1.0);
 
-	var tz2 = mat4(1.0, 0.0, 0.0, cubeSize2,
-		0.0, 1.0, 0.0, cubeSize2,
-		0.0, 0.0, 1.0, cubeSize2,
-		0.0, 0.0, 0.0, 1.0);
+    var tz2 = mat4(1.0, 0.0, 0.0, cubeSize2,
+        0.0, 1.0, 0.0, cubeSize2,
+        0.0, 0.0, 1.0, cubeSize2,
+        0.0, 0.0, 0.0, 1.0);
 
-	var looking = lookAt(vec3(cubeSize2, cubeSize2, 4 * cubeSize), vec3(cubeSize2, cubeSize2, 0), vec3(0, 1, 0));
-	var rotation = mult(rz, mult(ry, rx));
-	var modelView = mult(looking, mult(tz2, mult(rotation, tz1)));
-	var projection = perspective(50, aspect, depthMin, depthMax);
+    var looking = lookAt(vec3(cubeSize2, cubeSize2, 4 * cubeSize), vec3(cubeSize2, cubeSize2, 0), vec3(0, 1, 0));
+    var rotation = mult(rz, mult(ry, rx));
+    var modelView = mult(looking, mult(tz2, mult(rotation, tz1)));
+    var projection = perspective(50, aspect, depthMin, depthMax);
 
-	gl.uniformMatrix4fv(modelViewLoc, false, flatten(modelView));
-	gl.uniformMatrix4fv(projectionLoc, false, flatten(projection));
+    gl.uniformMatrix4fv(modelViewLoc, false, flatten(modelView));
+    gl.uniformMatrix4fv(projectionLoc, false, flatten(projection));
 
     var index = 0;
-	while (colorIndeces.length < indices.length) {
-		console.log("not all polygons have colors! setting them to something");
-		colorIndeces.push(0)
-	}
+    while (colorIndices.length < indices.length) {
+        console.log("not all polygons have colors! setting them to something");
+        colorIndices.push(0)
+    }
 
-	for (var i = 0; i < indices.length; i++) {
-		gl.uniform4fv(colorLoc, colors[colorIndeces[i]]);
-		gl.drawElements(gl.TRIANGLE_FAN, indices[i].length, gl.UNSIGNED_BYTE, index);
+    for (var i = 0; i < indices.length; i++) {
+        gl.uniform4fv(colorLoc, colors[colorIndices[i]]);
+        gl.drawElements(gl.TRIANGLE_FAN, indices[i].length, gl.UNSIGNED_BYTE, index);
         index += indices[i].length;
-	}
+    }
 
-	requestAnimFrame(render);
-};
+    requestAnimFrame(render);
+}
