4d73185b48a33720dac9214b6db53f69653bf114
fahslaj <fahslaj@rose-hulman.edu>
2016-10-11 21:54:10 -0400

    update logic to support getValidMoves() and update comments

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+// GameState variable to be watched by the UI
 var GameState = {
 	turn: 0,
 	bearing: 0,
@@ -25,6 +26,7 @@ var pieceMappy = {
 	18: [1, 1, 1, 1, 1]
 }
 
+// initialize GameState.board
 for (var i = 0; i < 24; i++) {
 	if (pieceMappy[i]) {
 		GameState.board.triangles[i] = pieceMappy[i];
@@ -33,29 +35,63 @@ for (var i = 0; i < 24; i++) {
 	}
 }
 
+/**
+ * Roll the die at the given diePosition and set its 'used' value to false.
+ */
 function rollDie(diePosition) {
 	GameState.dice[diePosition][0] = Math.floor(Math.random()*6) + 1;
 	GameState.dice[diePosition][1] = 0;
 }
 
+/**
+ * Return all valid moves for the current GameState.
+ */
+function getValidMoves() {
+	var player = GameState.turn;
+	var moves = [];
+	for (var startTriangle = 0; startTriangle < 24; startTriangle++) {
+		for (var endTriangle = startTriangle; endTriangle < 24; endTriangle++) {
+			if (isValidMove(startTriangle, endTriangle)) {
+				moves.push([startTriangle, endTriangle]);
+			}
+		}
+	}
+	return moves;
+}
+
+/**
+ * Returns 0 (false) if not a valid move.
+ * Returns 1 if is a valid move using die #1.
+ * Returns 2 if is a valid move using die #2.
+ * Returns 3 if is a valid move using both dice.
+ */
 function isValidMove(startTriangle, endTriangle) {
 	var player = GameState.turn;
 	if (GameState.board.triangles[startTriangle].length >= 1 &&
 		GameState.board.triangles[startTriangle].indexOf(player) != -1 &&
 		GameState.board.triangles[endTriangle].length <= 4 &&
 		GameState.board.triangles[endTriangle].indexOf(!player) == -1) {
-		if (endTriangle - startTriangle == GameState.dice[0][0] &&
-			!GameState.dice[0][1]) {
-			return 1;
-		}
 		if (endTriangle - startTriangle == GameState.dice[1][0] &&
 			!GameState.dice[1][1]) {
+			return 1;
+		}
+		if (endTriangle - startTriangle == GameState.dice[2][0] &&
+			!GameState.dice[2][1]) {
 			return 2;
-		} // TODO: check if uses both dice
+		} 
+		if (endTriangle - startTriangle == GameState.dice[1][0] + 
+			GameState.dice[2][0] && !GameState.dice[1][1] &&
+			!GameState.dice[2][1]) {
+			return 3;
+		}
 	}
 	return false;
 }
 
+/**
+ * Move one piece from startTriangle to the endTriangle. Moves the 
+ * current player's piece only, and will not make an invalid move.
+ */
 function makeMove(startTriangle, endTriangle) {
 	var die = isValidMove(startTriangle, endTriangle);
 	if (!die) {
@@ -63,13 +99,19 @@ function makeMove(startTriangle, endTriangle) {
 	}
 	GameState.board.triangles[endTriangle].push(
 		GameState.board.triangles[startTriangle].pop());
-	GameState.dice[die][1] = 1;
-	if (GameState.dice[0][1] && GameState.dice[1][1]) {
-		newTurn(); // TODO: change to next turn button
+	if (die == 3) {
+		GameState.dice[1][1] = 1;
+		GameState.dice[2][1] = 1;
+	} else {
+		GameState.dice[die][1] = 1;
 	}
 }
 
-function newTurn() {
+/**
+ * Switch to the other player's turn. 
+ * TODO: don't switch turns if a player can't legally end turn in the rules
+ */
+function endTurn() {
 	if (!checkWin()) {
 		rollDie(0);
 		rollDie(1);
@@ -77,6 +119,8 @@ function newTurn() {
 	}
 }
 
+/**
+ * Check for win conditions.
+ */
 function checkWin() {
-	// check for win conditions
 }
